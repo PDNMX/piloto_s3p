a9b35652210fa63d6276381dc2e4bc6f8fadaf8e
ecamargo-eng <ecamargo@txmglobal.com>
2020-09-11 10:32:28 -0500

    change type field monto

--- utils/models.js
+++ utils/models.js
@@ -84,7 +84,7 @@ let psancionadosSchema = new Schema({
     },
     documentos: { type: [], default: void 0 },
     multa:{
-        monto: String,
+        monto: Number,
         moneda: {
             clave:String,
             valor:String
